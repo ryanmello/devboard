2fe69763788e97da83a34674c3a6444bed472bbc
Ryan Mello <74434148+ryanmello@users.noreply.github.com>
2024-08-26 04:00:00 -0700

    shadcn

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,3 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      Hello World
+      <Button>HI!</Button>
+    </div>
+  );
 }
