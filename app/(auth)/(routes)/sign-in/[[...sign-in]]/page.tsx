7ea845121210943918ef6985364780942423e78d
Ryan Mello <74434148+ryanmello@users.noreply.github.com>
2024-09-02 15:49:02 -0700

    custom footer

--- app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
@@ -1,15 +1,26 @@
+import { Button } from "@/components/ui/button";
 import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
   return (
-    <SignIn
-      appearance={{
-        elements: {
-          footer: {
-            display: "none",
+    <div className="flex flex-col items-center justify-center">
+      <SignIn
+        appearance={{
+          elements: {
+            footer: {
+              display: "none",
+            },
           },
-        },
-      }}
-    />
+        }}
+      />
+      <div className="flex items-center mt-2">
+        <div className="cursor-default font-light">
+          <p>Don&apos;t have an account?</p>
+        </div>
+        <Button variant="link" className="px-2">
+          Sign up
+        </Button>
+      </div>
+    </div>
   );
 }
--- app/(auth)/(routes)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/(routes)/sign-up/[[...sign-up]]/page.tsx
@@ -1,15 +1,26 @@
+import { Button } from "@/components/ui/button";
 import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
   return (
-    <SignUp
-      appearance={{
-        elements: {
-          footer: {
-            display: "none",
+    <div className="flex flex-col items-center justify-center">
+      <SignUp
+        appearance={{
+          elements: {
+            footer: {
+              display: "none",
+            },
           },
-        },
-      }}
-    />
+        }}
+      />
+      <div className="flex items-center mt-2">
+        <div className="cursor-default font-light">
+          <p>Already have an account?</p>
+        </div>
+        <Button variant="link" className="px-2">
+          Sign in
+        </Button>
+      </div>
+    </div>
   );
 }
