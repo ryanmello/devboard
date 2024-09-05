4836309bce9aeaea1f417f099149efb99e7c57c2
Ryan Mello <74434148+ryanmello@users.noreply.github.com>
2024-09-05 04:00:00 -0700

    command

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,11 +1,39 @@
-import React from "react";
 import UserItem from "./UserItem";
+import {
+  Command,
+  CommandDialog,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandSeparator,
+  CommandShortcut,
+} from "@/components/ui/command";
 
 const Sidebar = () => {
   return (
     <div className="flex flex-col gap-4 w-[300px] min-w-[300px] border-r min-h-screen p-4">
       <UserItem />
-      <div className="grow">Menu</div>
+      <div className="grow">
+        <Command>
+          <CommandInput placeholder="Type a command or search..." />
+          <CommandList>
+            <CommandEmpty>No results found.</CommandEmpty>
+            <CommandGroup heading="Suggestions">
+              <CommandItem>Calendar</CommandItem>
+              <CommandItem>Search Emoji</CommandItem>
+              <CommandItem>Calculator</CommandItem>
+            </CommandGroup>
+            <CommandSeparator />
+            <CommandGroup heading="Settings">
+              <CommandItem>Profile</CommandItem>
+              <CommandItem>Billing</CommandItem>
+              <CommandItem>Settings</CommandItem>
+            </CommandGroup>
+          </CommandList>
+        </Command>
+      </div>
       <div>Settings</div>
     </div>
   );
