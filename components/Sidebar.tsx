5ab2136817b883a661276ebcdbb7beb356af8cce
Ryan Mello <74434148+ryanmello@users.noreply.github.com>
2024-09-06 04:00:00 -0700

    menu list

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,36 +1,71 @@
 import UserItem from "./UserItem";
 import {
   Command,
-  CommandDialog,
   CommandEmpty,
   CommandGroup,
-  CommandInput,
   CommandItem,
   CommandList,
   CommandSeparator,
-  CommandShortcut,
 } from "@/components/ui/command";
 
 const Sidebar = () => {
+  const menuList = [
+    {
+      group: "General",
+      items: [
+        {
+          link: "/",
+          text: "Profile",
+        },
+        {
+          link: "/inbox",
+          text: "Inbox",
+        },
+        {
+          link: "/billing",
+          text: "Billing",
+        },
+        {
+          link: "/notifications",
+          text: "Notifications",
+        },
+      ],
+    },
+    {
+      group: "Settings",
+      items: [
+        {
+          link: "/",
+          text: "Settings",
+        },
+        {
+          link: "/privacy",
+          text: "Privacy",
+        },
+        {
+          link: "/logs",
+          text: "Logs",
+        },
+      ],
+    },
+  ];
+
+  // https://www.youtube.com/watch?v=mju7e6Cf6Nk&list=PL8HkCX2C5h0VGhZnfbwf2hq7yD7nXMbJJ&index=3
+
   return (
     <div className="flex flex-col gap-4 w-[300px] min-w-[300px] border-r min-h-screen p-4">
       <UserItem />
       <div className="grow">
         <Command>
-          <CommandInput placeholder="Type a command or search..." />
           <CommandList>
-            <CommandEmpty>No results found.</CommandEmpty>
-            <CommandGroup heading="Suggestions">
-              <CommandItem>Calendar</CommandItem>
-              <CommandItem>Search Emoji</CommandItem>
-              <CommandItem>Calculator</CommandItem>
-            </CommandGroup>
+            {menuList.map((menu: any, key: number) => (
+              <CommandGroup key={key} heading={menu.group}>
+                {menu.items.map((option: any, optionKey: number) => (
+                  <CommandItem key={optionKey}>{option.text}</CommandItem>
+                ))}
+              </CommandGroup>
+            ))}
             <CommandSeparator />
-            <CommandGroup heading="Settings">
-              <CommandItem>Profile</CommandItem>
-              <CommandItem>Billing</CommandItem>
-              <CommandItem>Settings</CommandItem>
-            </CommandGroup>
           </CommandList>
         </Command>
       </div>
