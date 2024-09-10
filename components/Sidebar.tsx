3768af6521940920b2ee510e9787b1c5812d26ae
Ryan Mello <74434148+ryanmello@users.noreply.github.com>
2024-09-10 14:27:08 -0700

    sidebar icons

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,7 +1,7 @@
+import { Bell, CreditCard, Inbox, User } from "lucide-react";
 import UserItem from "./UserItem";
 import {
   Command,
-  CommandEmpty,
   CommandGroup,
   CommandItem,
   CommandList,
@@ -15,42 +15,27 @@ const Sidebar = () => {
       items: [
         {
           link: "/",
+          icon: <User />,
           text: "Profile",
         },
         {
           link: "/inbox",
+          icon: <Inbox />,
           text: "Inbox",
         },
         {
           link: "/billing",
+          icon: <CreditCard />,
           text: "Billing",
         },
         {
           link: "/notifications",
+          icon: <Bell />,
           text: "Notifications",
         },
       ],
     },
-    {
-      group: "Settings",
-      items: [
-        {
-          link: "/",
-          text: "Settings",
-        },
-        {
-          link: "/privacy",
-          text: "Privacy",
-        },
-        {
-          link: "/logs",
-          text: "Logs",
-        },
-      ],
-    },
   ];
-
-  // https://www.youtube.com/watch?v=mju7e6Cf6Nk&list=PL8HkCX2C5h0VGhZnfbwf2hq7yD7nXMbJJ&index=3
 
   return (
     <div className="flex flex-col gap-4 w-[300px] min-w-[300px] border-r min-h-screen p-4">
@@ -59,13 +44,15 @@ const Sidebar = () => {
         <Command>
           <CommandList>
             {menuList.map((menu: any, key: number) => (
-              <CommandGroup key={key} heading={menu.group}>
+              <CommandGroup key={key}>
                 {menu.items.map((option: any, optionKey: number) => (
-                  <CommandItem key={optionKey}>{option.text}</CommandItem>
+                  <CommandItem key={optionKey} className="flex gap-4 cursor-pointer">
+                    {option.icon}
+                    {option.text}
+                  </CommandItem>
                 ))}
               </CommandGroup>
             ))}
-            <CommandSeparator />
           </CommandList>
         </Command>
       </div>
