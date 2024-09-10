332aa3df64655ea41d9dae130bbcc0b9a1904839
Ryan Mello <74434148+ryanmello@users.noreply.github.com>
2024-09-10 15:10:45 -0700

    full user

---
+++ types/index.ts
@@ -0,0 +1,25 @@
+import { Education, Experience, Project } from "@prisma/client";
+
+export type FullUser = {
+  id: string;
+  clerkId: string;
+  email: string | null;
+  firstName: string | null;
+  lastName: string | null;
+  image: string | null;
+  headline: string | null;
+  resume: string | null;
+  role: string;
+  createdAt: Date;
+  updatedAt: Date;
+
+  gitHubUsername: string | null;
+  leetCodeUsername: string | null;
+  linkedInProfile: string | null;
+
+  skills: string[];
+
+  projects: Project[];
+  education: Education[];
+  experience: Experience[];
+};
