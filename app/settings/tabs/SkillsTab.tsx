cf21ddd0dc7116cf71f5d4b62a1d4ccb0e78cd59
Ryan Mello <74434148+ryanmello@users.noreply.github.com>
2024-09-29 13:31:12 -0700

    skills image

--- app/settings/tabs/SkillsTab.tsx
+++ app/settings/tabs/SkillsTab.tsx
@@ -1,12 +1,14 @@
-import { FullUser } from '@/types'
-import React from 'react'
+import { FullUser } from "@/types";
+import Languages from "@/public/languages.jpg";
+import Image from "next/image";
 
 const SkillsTab = ({ currentUser }: { currentUser: FullUser }) => {
   return (
     <div>
+      <Image src={Languages} alt="none" width={1000} height={1000} />
       Skills
     </div>
-  )
-}
+  );
+};
 
-export default SkillsTab
+export default SkillsTab;
