0cbc129248b87ac342d189ef736fe237043643b7
Ryan Mello <74434148+ryanmello@users.noreply.github.com>
2024-10-31 09:25:32 -0700

    hide skills

--- app/settings/tabs/SkillsTab.tsx
+++ app/settings/tabs/SkillsTab.tsx
@@ -1,11 +1,11 @@
 import { FullUser } from "@/types";
-import Languages from "@/public/languages.jpg";
+// import Languages from "@/public/languages.jpg";
 import Image from "next/image";
 
 const SkillsTab = ({ currentUser }: { currentUser: FullUser }) => {
   return (
     <div>
-      <Image src={Languages} alt="none" width={1000} height={1000} />
+      {/* <Image src={Languages} alt="none" width={1000} height={1000} /> */}
       Skills
     </div>
   );
