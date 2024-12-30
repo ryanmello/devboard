9458057de2545abc12934d9bba7fb22b712485c3
Ryan Mello <ryanmello897@gmail.com>
2024-12-30 14:28:47 -0800

    remove comments

--- app/actions/getCurrentUser.ts
+++ app/actions/getCurrentUser.ts
@@ -1,5 +1,4 @@
 import db from "@/lib/db";
-import { unstable_cache } from "next/cache";
 
 const getCurrentUser = async ({ clerkId }: { clerkId: string | null }) => {
   try {
@@ -27,36 +26,3 @@ const getCurrentUser = async ({ clerkId }: { clerkId: string | null }) => {
 };
 
 export default getCurrentUser;
-
-// const getCurrentUser = unstable_cache(
-//   async ({ clerkId }: { clerkId: string | null }) => {
-//     try {
-//       if (clerkId == null) return null;
-
-//       const currentUser = await db.user.findUnique({
-//         where: {
-//           clerkId: clerkId,
-//         },
-//         include: {
-//           projects: true,
-//           experience: true,
-//           education: true,
-//         },
-//       });
-
-//       if (!currentUser) {
-//         return null;
-//       }
-
-//       return currentUser;
-//     } catch (error: any) {
-//       return null;
-//     }
-//   },
-//   ["getCurrentUser"],
-//   {
-//     revalidate: 60,
-//   }
-// );
-
-// export default getCurrentUser;
