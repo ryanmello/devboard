adafccdb9257310f718b75c66dc27e598b0633e9
Ryan Mello <ryanmello897@gmail.com>
2025-01-01 11:39:40 -0800

    sidebar

--- app/[username]/components/LeftProfile.tsx
+++ app/[username]/components/LeftProfile.tsx
@@ -13,7 +13,7 @@ const LeftProfile = ({ user }: { user: FullUser }) => {
   const router = useRouter();
 
   return (
-    <div className="w-1/5">
+    <div className="">
       {user && (
         <div className="flex flex-col">
           <div className="w-full aspect-square relative">
