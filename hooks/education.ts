f408349f954f6511e07fba0100c3b8a38c8708dd
Ryan Mello <ryanmello897@gmail.com>
2025-02-14 13:09:00 -0800

    education hook

---
+++ hooks/education.ts
@@ -0,0 +1,48 @@
+export const useSacramentoColleges = () => {
+    const sacramentoColleges = [
+      {
+        id: 1,
+        name: "California State University, Sacramento",
+        image: "https://media.licdn.com/dms/image/v2/D560BAQG8h3rSTgRS5A/company-logo_100_100/company-logo_100_100/0/1692902976890/sacstate_logo?e=1747872000&v=beta&t=g0kBMaoUS_R2cWTPGZ-ghP8O09H-6O6vLgI7gHnL4Wo",
+      },
+      {
+        id: 2,
+        name: "University of California, Davis",
+        image: "https://media.licdn.com/dms/image/v2/C4E0BAQEBG25KNBwuCQ/company-logo_200_200/company-logo_200_200/0/1630629297217/uc_davis_logo?e=1747872000&v=beta&t=GoJp-a5Ilt5SlUc_l4Y3lpbvzoWuN4ARbYH7W7SDVzw",
+      },
+      {
+        id: 3,
+        name: "American River College",
+        image: "https://www.arc.losrios.edu/sites/main/files/main-images/arc-logo.png",
+      },
+      {
+        id: 4,
+        name: "Sacramento City College",
+        image: "https://www.scc.losrios.edu/sites/main/files/main-images/scc-logo.png",
+      },
+      {
+        id: 5,
+        name: "Folsom Lake College",
+        image: "https://www.flc.losrios.edu/sites/main/files/main-images/flc-logo.png",
+      },
+      {
+        id: 6,
+        name: "Cosumnes River College",
+        image: "https://www.crc.losrios.edu/sites/main/files/main-images/crc-logo.png",
+      },
+      {
+        id: 7,
+        name: "Sierra College",
+        image: "https://www.sierracollege.edu/themes/custom/sierracollege/logo.svg",
+      },
+      {
+        id: 8,
+        name: "William Jessup University",
+        image: "https://www.jessup.edu/wp-content/uploads/2021/01/Jessup-University.png",
+      },
+    ];
+  
+    return sacramentoColleges;
+  };
+
+  
