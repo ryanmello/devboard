ae42ab9047347b1fd6d7411b1599a3eacfd37c01
Ryan Mello <ryanmello897@gmail.com>
2025-02-15 13:29:26 -0800

    add images to education hook

--- hooks/education.ts
+++ hooks/education.ts
@@ -13,32 +13,32 @@ export const useSacramentoColleges = () => {
       {
         id: 3,
         name: "American River College",
-        image: "https://www.arc.losrios.edu/sites/main/files/main-images/arc-logo.png",
+        image: "https://media.licdn.com/dms/image/v2/C510BAQGbTmtmcMx3oA/company-logo_200_200/company-logo_200_200/0/1631322248543?e=1747872000&v=beta&t=sPW9Whk4BsIiSev6KtJiS8D1rXGwdjWjGS7EFqGKtpI",
       },
       {
         id: 4,
         name: "Sacramento City College",
-        image: "https://www.scc.losrios.edu/sites/main/files/main-images/scc-logo.png",
+        image: "https://media.licdn.com/dms/image/v2/C560BAQHPjDLGAwSCrg/company-logo_200_200/company-logo_200_200/0/1630642962898/sacramento_city_college_logo?e=1747872000&v=beta&t=DEqAEf276dRihBUdi2cM1pq1IiWvOeRiPX4cZPHQV4w",
       },
       {
         id: 5,
         name: "Folsom Lake College",
-        image: "https://www.flc.losrios.edu/sites/main/files/main-images/flc-logo.png",
+        image: "https://media.licdn.com/dms/image/v2/C4D0BAQGmU_dqydPzuQ/company-logo_200_200/company-logo_200_200/0/1631303127503?e=1747872000&v=beta&t=5Nzo8Qr-nUQMNxTnTGYZGDuQWqWEzwYWSmjkNpnit_0",
       },
       {
         id: 6,
         name: "Cosumnes River College",
-        image: "https://www.crc.losrios.edu/sites/main/files/main-images/crc-logo.png",
+        image: "https://media.licdn.com/dms/image/v2/C560BAQG6Jpou9SFNdQ/company-logo_200_200/company-logo_200_200/0/1630650142556/cosumnes_river_college_logo?e=1747872000&v=beta&t=72sqibcWfGIu42k07_rT-5--NSbtdogNxSpP69WCnWA",
       },
       {
         id: 7,
         name: "Sierra College",
-        image: "https://www.sierracollege.edu/themes/custom/sierracollege/logo.svg",
+        image: "https://media.licdn.com/dms/image/v2/C560BAQFQtJrof4b2LA/company-logo_200_200/company-logo_200_200/0/1631319732675?e=1747872000&v=beta&t=hZ7rKt6BesSaQ9C-SXuTXn4QzsUeoDt0ZvAWBf4j16s",
       },
       {
         id: 8,
         name: "William Jessup University",
-        image: "https://www.jessup.edu/wp-content/uploads/2021/01/Jessup-University.png",
+        image: "https://media.licdn.com/dms/image/v2/C560BAQGps_NG-iTNew/company-logo_200_200/company-logo_200_200/0/1630606045243/william_jessup_university_san_jose_logo?e=1747872000&v=beta&t=n5I4glnYvTi_4uK5qmjBpp2j75H6H_QGid-lyivFJ5k",
       },
     ];
   
